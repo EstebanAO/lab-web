1b94a961b44096b555dd2729a11fe3e296dbf29b
Esteban Arocha Ortuno <estebanarochaortuno@Estebans-MacBook-Pro-3.local>
2019-02-26 08:04:47 -0600

    Add log-in with devise gem to project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: "announcements#index"
   resources :announcements
   resources :documents
   resources :users
---
+++ db/migrate/20190226134850_remove_psw_from_user.rb
@@ -0,0 +1,5 @@
+class RemovePswFromUser < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :users, :password
+  end
+end
