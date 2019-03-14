7c77272721fff5bc50027beeef6b2eb81039aefb
Esteban Arocha <e.arocha@ukko.mx>
2019-03-14 13:11:47 -0600

    Create professor when create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
+    debugger
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
@@ -79,7 +79,8 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:password, :admin, :email)
+      debugger
+      params.require(:user).permit(:password, :admin, :email, :password_confirmation)
     end
 
     def check_admin
--- app/views/users/_user.json.jbuilder
+++ app/views/users/_user.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! user, :id, :password, :email, :admin, :created_at, :updated_at
+json.extract! user, :id, :password, :email, :admin, :password_confirmation, :created_at, :updated_at
 json.url user_url(user, format: :json)
