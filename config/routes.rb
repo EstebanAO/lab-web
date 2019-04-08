3e95a2c42b99b763d9d0c2992b639a214b5271bd
hector <hectorortiz_96@hotmail.com>
2019-04-08 15:19:44 -0500

    Agregar checkboxes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   root to: "professors_grid#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
-=======
-  root to: "professors_grid#index", data: { no_turbolink: true }
-  devise_for :users, :controllers => {:registrations => "registrations"}, :path => 'user_create'
-  resources :users
->>>>>>> 85d3cfecee15efda0d5d054283e019c1cf2f5479
   get '/professors/:id/show_card' => 'professors#show_card', :as => '/show_card'
+  get 'professors_grid', to: 'professors_grid#index'
+  resources :users
   resources :announcements
   resources :documents
   resources :professors
