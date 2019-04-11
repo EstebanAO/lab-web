0d03836fd7bac1be06d53038005d1f20c733fad9
hector <hectorortiz_96@hotmail.com>
2019-04-11 13:30:09 -0500

    Terminado busqueda por select

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,10 +8,15 @@ class HomeController < ApplicationController
   end
  
  def show_professor
+  if params[:search] == "Catedra"
+      search_i = 1
+    else if params[:search] == "Planta"
+      search_i = 0
+    end
+  end
   if params[:search]
-      @professors = Professor.where('name LIKE ? OR father_last_name LIKE ? OR mother_last_name LIKE ? OR professor_type LIKE ?', 
-        "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%")
-      debugger
+      @professors = Professor.where('name LIKE ? OR father_last_name LIKE ? OR mother_last_name LIKE ? OR professor_type = ?', 
+        "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", search_i)
     else
       @professors = Professor.all
     end
@@ -23,3 +28,5 @@ def professor_params
     params.require(:professor).permit(:name, :father_last_name, :mother_last_name, :professor_type, :search)
   end
 end
+
+ 
