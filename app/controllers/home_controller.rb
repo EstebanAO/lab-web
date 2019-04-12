0a6af8d33b75d851ba3b7a951a42277bacc55c11
Esteban Arocha <e.arocha@ukko.mx>
2019-04-11 23:41:58 -0500

    Fix: search and redirect to professor

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,28 +3,34 @@ class HomeController < ApplicationController
   def index
   end
 
-  def professor
+  def professors
     @professor = Professor.find(params[:id])
   end
- 
+
   def show_professor
-    if params[:search] == "Catedra"
-        search_i = 1
-      else if params[:search] == "Planta"
-        search_i = 0
-      end
+    # debugger
+    if params[:professor_type] == "Catedra" then
+      search_i = 0
+    elsif params[:professor_type] == "Planta"
+      search_i = 1
+    elsif  params[:professor_type] == "All"
+      search_i = 2
     end
-    if params[:search]
-      @professors = Professor.where('name LIKE ? OR father_last_name LIKE ? OR mother_last_name LIKE ? OR professor_type = ?', 
-        "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", search_i)
-      else
-        @professors = Professor.all
-      end
+    @ids = User.all.where(admin: false).map{|user| user.professor.id}
+    if params[:search] && search_i != 2
+      @professors = Professor.where(id: @ids).where('(lower(name) LIKE ? OR lower(father_last_name) LIKE ? OR lower(mother_last_name) LIKE ? ) and professor_type = ?',
+        "%#{params[:search].downcase}%", "%#{params[:search].downcase}%", "%#{params[:search].downcase}%", search_i)
+    elsif params[:search] && search_i == 2
+      @professors = Professor.where(id: @ids).where('lower(name) LIKE ? OR lower(father_last_name) LIKE ? OR lower(mother_last_name) LIKE ? ',
+        "%#{params[:search].downcase}%", "%#{params[:search].downcase}%", "%#{params[:search].downcase}%")
+    else
+      @professors = Professor.where(id: @ids)
+    end
   end
 
 private
 
 def professor_params
-    params.require(:professor).permit(:name, :father_last_name, :mother_last_name, :search)
+    params.require(:professor).permit(:name, :father_last_name, :mother_last_name, :search, :professor_type)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}, :path => 'user_create'
   resources :users
-  get '/home/:id/professor' => 'home#professor', :as => '/professor'
+  get '/home/professors/:id' => 'home#professors', :as => '/home/professors'
   get '/home/show_professor' => 'home#show_professor', :as => '/show_professor'
 
   resources :announcements
