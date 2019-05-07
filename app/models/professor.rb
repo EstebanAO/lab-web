0accf8528fe5b8cc7d3a98ae6c78dfc223131d54
Esteban Arocha <e.arocha@ukko.mx>
2019-05-06 19:38:04 -0500

    add link style to resume in professor

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -3,4 +3,19 @@ class Professor < ApplicationRecord
   has_many :announcements, :dependent => :destroy
   has_many :documents, :dependent => :destroy
   enum professor_type: %i[Planta Catedra]
+
+  before_validation :validate_https
+
+  def validate_https
+    url_cpy_with_s = self.resume
+    url_cpy_with_out_s = self.resume
+    url_cpy_with_s = url_cpy_with_s[0...8]
+    url_cpy_with_out_s = url_cpy_with_out_s[0...7]
+    if url_cpy_with_s == "https://" then
+      self.resume = self.resume[8...]
+    end
+    if url_cpy_with_out_s == "http://" then
+      self.resume = self.resume[7...]
+    end
+  end
 end
