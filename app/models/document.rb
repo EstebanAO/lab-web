5f8e28c3978c7fc68a76c6639936cd483e9be75d
Esteban Arocha <e.arocha@ukko.mx>
2019-05-06 19:33:38 -0500

    Add link click and verification to documents and links

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,18 @@
 class Document < ApplicationRecord
   belongs_to :professor
+  
+  before_validation :validate_https
+
+  def validate_https
+    url_cpy_with_s = self.url
+    url_cpy_with_out_s = self.url
+    url_cpy_with_s = url_cpy_with_s[0...8]
+    url_cpy_with_out_s = url_cpy_with_out_s[0...7]
+    if url_cpy_with_s == "https://" then
+      self.url = self.url[8...]
+    end
+    if url_cpy_with_out_s == "http://" then
+      self.url = self.url[7...]
+    end
+  end
 end
